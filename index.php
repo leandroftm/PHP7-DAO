59944e9f3da252fc04578a739160cd828936b633
Leandro Torres <leandroftm@gmail.com>
2018-11-07 16:57:12 -0200

    classe usuario criada

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
 	require_once('config.php');
 
+	/*
+
 	$sql = new Sql();
 
 	$usuarios = $sql->select('SELECT * FROM tb_usuarios');
 
 	echo json_encode($usuarios);
+	*/
+
+	$user = new Usuario();
+
+	$user->loadById(1);
+
+	echo $user;
 
  ?>
